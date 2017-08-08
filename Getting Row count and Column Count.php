5aa6a9c07c44c8a4923cb02149fc56d02326412d
Ahmed Salah <GeekAhmeds@Gmail.com>
2017-08-08 04:35:04 +0200

    #2

---
+++ Getting Row count and Column Count.php
@@ -0,0 +1,11 @@
+<?php
+try {
+/// To Connect To Database With PDO
+   ///
+   $pdo = new PDO('mysql:host=localhost;dbname=auth','root','',[
+    PDO::ATTR_EMULATE_PREPARES => false,
+    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+  ]);
+} catch (PDOException $e) {
+  print_r($e->getMessage());
+}
