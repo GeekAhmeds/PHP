5b1ac0c9cc8cbb000f1755106609161d17941b05
Ahmed Salah <GeekAhmeds@Gmail.com>
2017-08-08 04:59:46 +0200

    #2

--- PDO Samples/Getting Row count and Column Count.php
+++ PDO Samples/Getting Row count and Column Count.php
@@ -4,8 +4,23 @@ try {
    ///
    $pdo = new PDO('mysql:host=localhost;dbname=auth','root','',[
     PDO::ATTR_EMULATE_PREPARES => false,
-    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
   ]);
+  $pdo->exec('SET names utf8');
+
 } catch (PDOException $e) {
   print_r($e->getMessage());
 }
+$stmt = $pdo->prepare('SELECT username FROM users WHERE username=:username');
+$stmt->execute([
+'username' => 'ahmed'
+]);
+if ($stmt->rowCount()) {
+  foreach ($stmt->fetchALL() as $value) {
+    print_r($value);
+  }
+}else {
+  echo "No Such User";
+}
+echo $stmt->columnCount();
