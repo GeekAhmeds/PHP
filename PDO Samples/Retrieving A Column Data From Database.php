fc807f7a59f4f67074fc1f17219c64a5ed32c56e
Ahmed Salah <GeekAhmeds@Gmail.com>
2017-08-08 04:22:19 +0200

    #1

--- PDO Samples/Retrieving A Column Data From Database.php
+++ PDO Samples/Retrieving A Column Data From Database.php
@@ -2,12 +2,20 @@
 try {
 /// To Connect To Database With PDO
    ///
-  $pdo = new PDO('mysql:host=localhost;dbname=auth','root','',[
+   $pdo = new PDO('mysql:host=localhost;dbname=auth','root','',[
+    PDO::ATTR_EMULATE_PREPARES => false,
     PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
-
   ]);
-
-
 } catch (PDOException $e) {
   print_r($e->getMessage());
+}
+
+//To Understand It Sersh In Manuale PHP.net
+$stmt = $pdo->prepare('SELECT * FROM users WHERE username = "ahmed"');
+$stmt->execute();
+$stmt->bindColumn(1, $username);
+$stmt->bindColumn(2, $password);
+// Make While Loop For Fetch Data Receved From DB Using bindColumn();
+while ($stmt->fetchAll(PDO::FETCH_BOUND)) {
+  echo "HELLO " . $username . 'Your Password ' . $password;
 }
